9e928706b944408a19e98b1eb9f82f599809a32b
onderonur <onuronder.onr@gmail.com>
2020-01-27 01:35:14 +0300

    refactor

--- api/src/resolvers/index.ts
+++ api/src/resolvers/index.ts
@@ -5,20 +5,20 @@ import scalars from './scalars';
 import merge from 'lodash/merge';
 import queries from './queries';
 
+const mutationResponse: Resolvers<GQLContext>['MutationResponse'] = {
+  __resolveType: () => {
+    // TODO: This __resolveType implementation may need a fix.
+    // Need to check how to use it with TypeScript (or graphql-codegen).
+    return null;
+  },
+};
+
 const resolvers: Resolvers<GQLContext> = merge(
   {},
   scalars,
   queries,
   mutations,
-  {
-    MutationResponse: {
-      __resolveType: () => {
-        // TODO: This __resolveType implementation may need a fix.
-        // Need to check how to use it with TypeScript (or graphql-codegen).
-        return null;
-      },
-    },
-  },
+  mutationResponse,
 );
 
 export default resolvers;
