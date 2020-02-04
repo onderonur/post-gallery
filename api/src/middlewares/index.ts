9a7f76edf40ad84cc662fa066b70e08930856643
onderonur <onuronder.onr@gmail.com>
2020-02-04 21:13:38 +0300

    fix

--- api/src/middlewares/index.ts
+++ api/src/middlewares/index.ts
@@ -8,6 +8,9 @@ export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   return res.status(status).json({ status, message });
 };
 
+// About using csrf tokens with csurf;
+// https://stackoverflow.com/questions/59606019/express-js-csurf-cookie-and-header-match-returning-403/59607800#59607800
+// https://stackoverflow.com/questions/59547873/how-to-secure-my-react-app-api-with-csurf/59549909#59549909
 export const passCsrfTokenToClient: RequestHandler = (req, res, next) => {
   res.cookie('csrfToken', req.csrfToken());
   return next();
