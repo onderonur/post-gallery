5f591aaf04a8c2bd6898c425de4c88d305bc2ad6
onderonur <onuronder.onr@gmail.com>
2020-04-19 05:17:14 +0300

    ux improvement

--- frontend/src/views/Login/components/GoogleLoginButton.tsx
+++ frontend/src/views/Login/components/GoogleLoginButton.tsx
@@ -1,31 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import SocialLoginButton from "./SocialLoginButton";
 import { colors } from "@material-ui/core";
 import GoogleIcon from "./GoogleIcon";
 import GoogleLogin, { GoogleLoginResponse } from "react-google-login";
 import axios from "axios";
 import { redirectToHome } from "@/utils";
+import VerifiedLoginInfo from "./VerifiedLoginInfo";
 
 const GoogleLoginButton = () => {
+  const [isLoginVerified, setIsLoginVerified] = useState<boolean>(false);
   return (
     <GoogleLogin
       clientId={process.env.GOOGLE_OAUTH_CLIENT_ID}
       render={({ onClick, disabled }) => (
-        <SocialLoginButton
-          providerName="Google"
-          icon={<GoogleIcon />}
-          backgroundColor={colors.common.white}
-          fontColor="#737373"
-          activeBackgroundColor="#e5e5e5"
-          disabled={disabled}
-          onClick={onClick}
-        />
+        <>
+          <SocialLoginButton
+            providerName="Google"
+            icon={<GoogleIcon />}
+            backgroundColor={colors.common.white}
+            fontColor="#737373"
+            activeBackgroundColor="#e5e5e5"
+            disabled={disabled}
+            onClick={onClick}
+          />
+          <VerifiedLoginInfo isVerified={isLoginVerified} />
+        </>
       )}
       onSuccess={(response) => {
         const idToken = (response as GoogleLoginResponse).getAuthResponse()
           .id_token;
         axios.post("/api/auth/google", { idToken }).then(({ data }) => {
-          if (data.verified) {
+          const { verified } = data;
+          setIsLoginVerified(verified);
+          if (verified) {
             redirectToHome();
           }
         });
---
+++ frontend/src/views/Login/components/VerifiedLoginInfo.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+interface VerifiedLoginInfoProps {
+  isVerified: boolean;
+}
+
+const VerifiedLoginInfo: React.FC<VerifiedLoginInfoProps> = ({
+  isVerified,
+}) => {
+  if (!isVerified) {
+    return null;
+  }
+
+  return (
+    <Typography color="primary">
+      You are now logged in and will be automatically redirected.
+    </Typography>
+  );
+};
+
+export default VerifiedLoginInfo;
